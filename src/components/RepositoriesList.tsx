822e9dd04ba2f4368833b31e76e165d952e45ca8
Robert Baddeley <robertbaddeley@gmail.com>
2021-05-06 06:18:47 +0900

    wrapped up redux, wiring in react

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
