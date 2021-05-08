ee7787032696d238aafc86f2a5cb47753ac40c6b
Robert Baddeley <robertbaddeley@gmail.com>
2021-05-09 04:41:20 +0900

    finished redux portion. its 'done'

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -22,6 +22,8 @@ const RepositoriesList: React.FC = () => {
         <button>Search</button>
       </form>
       {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
